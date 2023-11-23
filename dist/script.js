f4680329bc614f880ab5ed9a57cc6af3047b95bf
Cung-Te <jmikthang5@gmail.com>
2023-11-22 20:47:03 -0500

    initial commit

--- dist/script.js
+++ dist/script.js
@@ -2,6 +2,6 @@ const toggle = document.getElementById('toggle');
 const nav = document.getElementById('nav');
 
 toggle.addEventListener('click', () => {
-
+      console.log(nav)
       nav.classList.toggle('active');
 })
