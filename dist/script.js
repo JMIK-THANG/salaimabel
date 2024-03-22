018df792b7a0542ee2fd6a1910524164fc552c1d
Cung-Te <jmikthang5@gmail.com>
2024-03-21 20:55:40 -0400

    initial commit

--- dist/script.js
+++ dist/script.js
@@ -5,3 +5,23 @@ toggle.addEventListener('click', () => {
       console.log(nav)
       nav.classList.toggle('active');
 })
+
+// const mainMenu = document.querySelector('.mainMenu');
+// const closeMenu = document.querySelector('.closeMenu');
+// const openMenu = document.querySelector('.openMenu');
+
+
+
+// openMenu.addEventListener('click',show);
+// closeMenu.addEventListener('click',close);
+// service.addEventListener('click',service);
+
+// function show(){
+//       mainMenu.style.display = 'flex';
+//       mainMenu.style.top = '0';
+
+// }
+
+// function close(){
+//       mainMenu.style.top = '-110%'
+// }
